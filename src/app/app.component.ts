7b76d306f60c286c9d9bb86d3badeed8846c8d3b
shivamgumber <Shivammaangumber@gmail.com>
2020-07-18 11:36:00 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {CoronaService} from './corona.service';
+import {GoogleChartInterface} from 'ng2-google-charts';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,71 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'covidtracker';
-}
+  countries: any;
+  country: any;
+  Confirmed: number;
+  Recovered: number;
+  Deaths: number;
+  Date: Date;
+  Active: number;
+  Country: string;
+  TotalConfirmed: number;
+  TotalRecovered: number;
+  TotalDeaths: number;
+  pieChart: GoogleChartInterface = {
+    chartType: 'PieChart'
+  };
+
+
+
+  constructor(private corona: CoronaService){}
+  ngOnInit(){
+    this.corona.getCountries().subscribe((data) => {
+      console.log(data);
+      this.countries = data;
+
+
+    }
+    );
+    this.getWorldtotal();
+    this.updateChart();
+
+  }
+  // tslint:disable-next-line:typedef
+
+  getCoronaData(){
+    this.corona.getCoronaRealData(this.country).subscribe((data) => {
+      console.log(data);
+      const index = data.length - 1;
+      this.Confirmed = data[index].Confirmed;
+      this.Recovered = data[index].Recovered;
+      this.Deaths = data[index].Deaths;
+      this.Date = data[index].Date;
+      this.Active = data[index].Active;
+      this.Country = data[index].Country;
+    });
+  }
+  getCountry(country: any){
+    this.country = country;
+  }
+  getWorldtotal(){
+    this.corona.getTotal().subscribe((data) => {
+      console.log(data);
+      this.TotalConfirmed = data.TotalConfirmed;
+      this.TotalDeaths = data.TotalDeaths;
+      this.TotalRecovered = data.TotalRecovered;
+    });
+    }
+    updateChart(){
+    const dataTable = [];
+    
+
+
+    }
+  }
+
+
+
+
+
+
